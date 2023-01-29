1a0a15de725fc77b737e9e7564bdcfbbab236f74
AravindSI <aravindsivankalai99>
2023-01-29 18:06:33 +0530

    react one

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem(){
+    return <h2> ExpenseItem! 
+        <div>
+        Food Rs 10
+        </div>
+        <div>
+        Petrol Rs 100
+        </div>
+        <div>
+        ovies Rs 200
+        </div>
+       </h2>
+           
+}
+export default ExpenseItem;
