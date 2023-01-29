1767b2dd5899b41e6cd01c285d39f26a6f982813
AravindSI <aravindsivankalai99>
2023-01-29 18:24:03 +0530

    a

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,11 @@
-function ExpenseItem(){
-    return <h2> ExpenseItem! 
-        <div>
-        Food Rs 10
-        </div>
-        <div>
-        Petrol Rs 100
-        </div>
-        <div>
-        ovies Rs 200
-        </div>
-       </h2>
-           
+function ExpenseItem() {
+  return (
+    <div>
+      <h2> ExpenseItem!</h2>
+      <div>Food Rs 10</div>
+      <div>Petrol Rs 100</div>
+      <div>movies Rs 200</div>
+    </div>
+  );
 }
 export default ExpenseItem;
